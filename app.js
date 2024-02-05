6b02cf374ec8064ac0b3b46ee4754bf56b7741a0
Nikoooh <nikotoivanen1999@gmail.com>
2024-02-05 17:18:07 +0200

    osa4-a valmis

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogRouter = require('./controllers/blogRouter')
+const config = require('./utils/config')
+
+mongoose.connect(config.MONGODB_URI)
+
+app.use(cors())
+app.use(express.json())
+
+app.use(blogRouter)
+
+module.exports = app
